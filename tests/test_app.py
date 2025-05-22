2cc458103b1171fa93323f2a3be8ada759681e3a
GraceOnuoha <grace.onuoha@gmail.com>
2025-05-22 12:07:34 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app import app
 
 def test_hello():
-    client = app.test.client()
+    tester = app.test.client()
     response = tester.get("/")
     assert response.status_code == 200
     assert b'Hello, Ivanti!' in response.data
